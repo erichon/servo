3a9807ed6e2d7486e7abe181e9246fbaeb199478
erichon <erichon18@gmail.com>
2021-03-18 01:10:05 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,37 @@
-pins.servoWritePin(AnalogPin.P0, 0)
+pins.servoSetPulse(AnalogPin.P0, 3000)
+let angle = 3
+let direction = 1
+basic.showLeds(`
+    . . # . .
+    . # . . .
+    # # # # #
+    . # . . .
+    . . # . .
+    `)
 basic.forever(function () {
-    pins.servoWritePin(AnalogPin.P0, 80)
-    basic.pause(500)
-    pins.servoWritePin(AnalogPin.P0, -90)
-    basic.pause(500)
+    pins.servoWritePin(AnalogPin.P0, angle)
+    if (angle >= 180) {
+        direction = -1
+        angle += direction
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    }
+    if (angle <= 3) {
+        direction = 1
+        angle += direction
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    }
+    angle += direction
+    basic.pause(100)
 })
