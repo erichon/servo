9f5ade34fab58cec77eeab47896ef23e813abdec
erichon <erichon18@gmail.com>
2021-03-17 19:04:16 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
+pins.servoWritePin(AnalogPin.P0, 0)
 basic.forever(function () {
-    pins.servoWritePin(AnalogPin.P0, 180)
-    basic.pause(2000)
-    pins.servoWritePin(AnalogPin.P0, 120)
-    basic.pause(2000)
+    pins.servoWritePin(AnalogPin.P0, 80)
+    basic.pause(500)
+    pins.servoWritePin(AnalogPin.P0, -90)
+    basic.pause(500)
 })
