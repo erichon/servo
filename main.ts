89fb6f0ea8c70df68213b25fe4cad44da8630a54
erichon <erichon18@gmail.com>
2021-03-18 11:16:41 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 pins.servoSetPulse(AnalogPin.P0, 3000)
-let speed = 5
-let min_angle = 8
+let speed = 1
+let min_angle = 45
+let max_angle = 135
 let angle = min_angle
 let direction = speed
 basic.showLeds(`
@@ -12,7 +13,7 @@ basic.showLeds(`
     `)
 basic.forever(function () {
     pins.servoWritePin(AnalogPin.P0, angle)
-    if (angle >= 180) {
+    if (angle >= max_angle) {
         direction = speed * -1
         angle += direction
         basic.showLeds(`
@@ -23,7 +24,7 @@ basic.forever(function () {
             . . # . .
             `)
     }
-    if (angle <= 8) {
+    if (angle <= min_angle) {
         direction = speed
         angle += direction
         basic.showLeds(`
