a0407f9eacb73a009179c1fc9ef3d6178a910fab
erichon <erichon18@gmail.com>
2021-05-24 11:04:00 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,16 +5,6 @@ let max_angle = 135
 let angle = min_angle
 let direction = speed
 basic.forever(function () {
-    pins.analogSetPeriod(AnalogPin.P0, 20000)
-    pins.servoSetPulse(AnalogPin.P0, 1300)
-    basic.pause(1000)
-    pins.analogSetPeriod(AnalogPin.P0, 20000)
-    pins.servoSetPulse(AnalogPin.P0, 1500)
-    basic.pause(2000)
-    pins.analogSetPeriod(AnalogPin.P0, 20000)
-    pins.servoSetPulse(AnalogPin.P0, 1700)
-    basic.pause(1000)
-    pins.analogSetPeriod(AnalogPin.P0, 20000)
-    pins.servoSetPulse(AnalogPin.P0, 1500)
-    basic.pause(2000)
+    pins.servoSetPulse(AnalogPin.P0, 5)
+    basic.pause(500)
 })
